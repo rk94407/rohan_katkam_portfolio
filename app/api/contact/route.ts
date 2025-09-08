d91d2f8442fba8b1bebedd7eeb5912e9851fad02
Rohan <rk944@njit.edu>
2025-09-08 17:38:37 -0400

    Updated code

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -10,9 +10,9 @@ export async function POST(req: Request) {
 
     await resend.emails.send({
       from: "Rohan Katkam <rohankatkam@resend.dev>", // must be verified in Resend
-      to: `${process.env.EMAIL_ADDRESS} || 'rohankatkam1698@gmail.com `, // ✅ your email
+      to: process.env.EMAIL_ADDRESS || "rohankatkam1698@gmail.com", // ✅ fallback to Gmail
       subject: `New message from ${name}`,
-      html: portfolioMessageTemplate(name, email, message), // ✅ use template
+      html: portfolioMessageTemplate(name, email, message),
     });
 
     return NextResponse.json({ success: true });
