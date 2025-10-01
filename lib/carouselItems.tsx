dcca7a1a7b8281ece1d6e43216fda1f9640a3cdd
Rohan <rk944@njit.edu>
2025-10-01 01:00:19 -0400

    Add new project on carousel

--- lib/carouselItems.tsx
+++ lib/carouselItems.tsx
@@ -10,22 +10,33 @@ export const DEFAULT_ITEMS: CarouselItem[] = [
   {
     id: 1,
     title: "Movie App",
-    description: "Movie App built with React, react-router-dom, Tailwind CSS., and TMDB API.",
+    description:
+      "Movie App built with React, react-router-dom, Tailwind CSS., and TMDB API.",
     image: "/images/movie-app.png",
     link: "https://react-movie-hub-app-github.netlify.app",
   },
   {
     id: 2,
     title: "Bio187.com",
-    description: "Bio187 is built with React, Next.js, Tailwind CSS, and MySQL.",
+    description:
+      "Bio187 is built with React, Next.js, Tailwind CSS, and MySQL.",
     image: "/images/bio187.png",
     link: "https://bio187.com/",
   },
   {
     id: 3,
     title: "Rite2Rule.com",
-    description: "Rite2Rule is built with React, TypeScript, Next.js, Tailwind CSS, Node.js, Express.js, and MySQL.",
+    description:
+      "Rite2Rule is built with React, TypeScript, Next.js, Tailwind CSS, Node.js, Express.js, and MySQL.",
     image: "/images/rite2rule.png",
     link: "https://rite2rule.com/",
+  },
+  {
+    id: 4,
+    title: "RAG AI Chatbot",
+    description:
+      "AI RAG Chatbot built with langchain, clerk, neon(postgres), openai, ai-sdk, and nextjs.",
+    image: "/images/ai-rag-chatbot.png",
+    link: "https://github.com/rk94407/ai_rag_chatbot",
   },
 ];
